89ad3ffd806e85bd268da4dc8d5c615b3cf5d11c
Sergey Kiselev <s79817066666@gmail.com>
2023-02-20 23:51:36 +0300

    created Jaeger and Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String sex = "Man";
+    String name = "Frank";
+    float height = 2.1f;
+    double weight = 95.4;
+    int age = 35;
+
+    public void move() {
+        System.out.println("moving");
+    }
+
+    public void sit() {
+        System.out.println("sitting");
+    }
+
+    public void run() {
+        System.out.println("running");
+    }
+
+    public void speak() {
+        System.out.println("speaking");
+    }
+
+    String learnJava() {
+        return "Learns Java";
+    }
+}
